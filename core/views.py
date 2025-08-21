05278725f13a41073225162f67c74b8c86e8906f
Nik <your@mail.com>
2025-08-21 20:55:47 +0200

    Add index view with stats for studios, workers and games

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from core.models import Studio, Worker, Game
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_studios = Studio.objects.count()
+    num_worker = Worker.objects.count()
+    num_game = Game.objects.count()
+
+    context = {
+        'num_studios': num_studios,
+        'num_worker': num_worker,
+        'num_game': num_game,
+    }
+
+    return render(request,'core/index.html', context=context)
