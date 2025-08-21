34e70977b7bf0481b01e5f8f2486a8e8c092cd6f
Nik <your@mail.com>
2025-08-21 21:10:47 +0200

    Add index view with stats for studios, workers and games

--- core/views.py
+++ core/views.py
@@ -6,13 +6,13 @@ from core.models import Studio, Worker, Game
 
 def index(request: HttpRequest) -> HttpResponse:
     num_studios = Studio.objects.count()
-    num_worker = Worker.objects.count()
-    num_game = Game.objects.count()
+    num_workers = Worker.objects.count()
+    num_games = Game.objects.count()
 
     context = {
         'num_studios': num_studios,
-        'num_worker': num_worker,
-        'num_game': num_game,
+        'num_worker': num_workers,
+        'num_game': num_games,
     }
 
     return render(request,'core/index.html', context=context)
