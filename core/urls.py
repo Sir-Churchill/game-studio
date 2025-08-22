1c9a7a0739d3d77e4b791790547237f23f42f135
Nik <your@mail.com>
2025-08-22 21:49:27 +0200

    Add list and detail views for studios, workers, and games to core URLs

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 
-from .views import index
+from .views import (index,
+                    StudioListView,
+                    GameListView,
+                    WorkerListView,
+                    StudioDetailView,
+                    WorkerDetailView
+                    )
 
 urlpatterns = [
     path('', index, name='index'),
+    path('studios/', StudioListView.as_view(), name='studio-list'),
+    path('studios/<int:pk>/', StudioDetailView.as_view(), name='studio-detail'),
+    path('workers/', WorkerListView.as_view(), name='worker-list'),
+    path('workers/<int:pk>/', WorkerDetailView.as_view(), name='worker-detail'),
+    path('games/', GameListView.as_view(), name='game-list'),
+
 ]
 
 app_name = 'core'
