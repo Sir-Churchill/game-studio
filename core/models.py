eecf4a6242942baa06460c7d112671dd622cadf7
Nik <your@mail.com>
2025-08-23 20:41:20 +0200

    feat(models): add __str__ for Position and get_absolute_url for Worker
    - Added __str__ method to Position to return its name.
    - Added get_absolute_url method to Worker to provide direct link to worker detail page.

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class Studio(models.Model):
@@ -16,6 +17,8 @@ class Studio(models.Model):
 class Position(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self) -> str:
+        return self.name
 
 class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name='workers')
@@ -27,6 +30,9 @@ class Worker(AbstractUser):
 
     def __str__(self) -> str:
         return f"{self.position.name}: {self.first_name} {self.last_name}"
+
+    def get_absolute_url(self):
+        return reverse("core:worker-detail", kwargs={"pk": self.pk})
 
 class Game(models.Model):
     name = models.CharField(max_length=255)
